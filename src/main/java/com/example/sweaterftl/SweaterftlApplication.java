83331ed262a0365c04643dc64826e02541d4c583
V.V.Kravchenko <kravczek@gmail.com>
2021-02-24 11:08:27 +0200

    First Spring commit Hello Vyacheslav ftl

---
+++ src/main/java/com/example/sweaterftl/SweaterftlApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweaterftl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterftlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweaterftlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sweaterftl/SweaterftlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweaterftl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterftlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
